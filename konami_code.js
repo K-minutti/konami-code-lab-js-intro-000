b8b0fa3113a0dc6e265640e566bf71e883f6f7d4
Kevin Minutti <minutti70@gmail.com>
2020-03-29 22:24:15 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,64 +1,18 @@
-// const codes = [
-//   "ArrowUp",
-//   "ArrowUp",
-//   "ArrowDown",
-//   "ArrowDown",
-//   "ArrowLeft",
-//   "ArrowRight",
-//   "ArrowLeft",
-//   "ArrowRight",
-//   "b",
-//   "a"
-// ];
-//
-//   // if the keydown even sequence is === to the codes array
-//   // return alert "Congrats"
-//   // else {
-//   //   keep listening
-//   // }
-// }
-//
-// function init (){
-//   document.bodyaddEventListener("keydown", (event) => {
-//
-//     let index = 0;
-//
-//     function onKeyDownHandler(e) {
-//       const key = e.key;
-//
-//       if (key === codes[index]) {
-//         index++;
-//
-//         if (index === codes.length) {
-//           alert("Hurray!");
-//
-//           index = 0
-//         }
-//       } else {
-//          index = 0; }
-//     }
-//   })
-// }
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
 
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-const codes = ['a', 'b', 'c'];
+function init() {
 
-// Keep track of index outside of the event handler.
-let index = 0;
-
-// This is the function that would be invoked by the event listener.
-function onKeyDownHandler(e) {
-  const key = e.key;
-
-  if (key === codes[index]) {
-    index++;
-
-    if (index === codes.length) {
-      alert("Hurray!");
-
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
 }
