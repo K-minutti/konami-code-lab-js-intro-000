277067048e5365bb8afaaabeedfa45b8a25ea597
Kevin Minutti <minutti70@gmail.com>
2020-03-29 22:28:17 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,8 +11,13 @@ const codes = [
   "a"
 ];
 
+event.key are down there
+
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+document.body.addEventListener("keydown", (event) => {
 
+}
+);
 }
